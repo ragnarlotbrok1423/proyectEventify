ea31ba668b9cc00ed551a2046037a5b1b71fe3e9
erick <erick.gonzalez13@utp.ac.pa>
2024-06-21 11:33:52 -0500

    instalaciones
    se instalaron recursos y se organizaron las carpeta para la arquitectura VIPER

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0-beta03")
 }
