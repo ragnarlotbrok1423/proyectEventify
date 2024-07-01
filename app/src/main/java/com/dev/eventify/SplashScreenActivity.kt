a68bcadfc9e29b964ba4811ec4db7bd31443f438
Giulolo <isabel.lo@utp.ac.pa>
2024-07-01 02:36:55 -0500

    fix repetitive splash screen problem

--- app/src/main/java/com/dev/eventify/SplashScreenActivity.kt
+++ app/src/main/java/com/dev/eventify/SplashScreenActivity.kt
@@ -1,35 +1,35 @@
 package com.dev.eventify
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.dev.eventify.ui.themes.EventifyTheme
-import com.dev.eventify.views.OnboardingScreenView
-
+import com.dev.eventify.views.SplashScreenView
 @SuppressLint("CustomSplashScreen")
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity: ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // true asi que el activity mueve al otro activity
         setContent{
             EventifyTheme(darkTheme = false) {
                 Surface(
                     modifier= Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    SplashScreenView()
 
-                    OnboardingScreenView()
-//                    LoginScreenView()
-//                    RegisterScreenView()
                 }
             }
 
         }
     }
+
 }
+
