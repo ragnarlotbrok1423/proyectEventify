d1b960b757d59c843943d4f14d091ed99e0e85d6
erick <erick.gonzalez13@utp.ac.pa>
2024-07-04 12:27:36 -0500

    intalando dependencias para usar conexiones con la api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,4 +109,17 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata")
     //  Integration with RxJava
     implementation("androidx.compose.runtime:runtime-rxjava2")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
+    //coil
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+
+    //material design
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0-beta03")
 }
