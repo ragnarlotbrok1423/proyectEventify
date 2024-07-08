c882e1c851e1ef8242d08d521e91f3c426d4ed85
Giulolo <isabel.lo@utp.ac.pa>
2024-07-07 20:27:11 -0500

    organized navigation and inputs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,6 @@ dependencies {
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.1")//2.7.1
     implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
-//    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     // Android Studio Preview support
     implementation("androidx.compose.ui:ui-tooling-preview")
